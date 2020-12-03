5f70b1e264cd607619a9077f5942b83636705821
Dimitar Vassilev <d.v.vassilev@gmail.com>
2020-12-03 19:42:13 +0200

    feat/ added models and raouter

---
+++ Workshop-router/ng-workshop-componenet/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+//import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+
+
+const routes: Routes = [
+    {
+        path: "",
+        pathMatch: 'full',
+        component: HomeComponent
+    }
+];
+
+export const AppRoutingModule = RouterModule.forRoot(routes);
--- Workshop-router/ng-workshop-componenet/src/app/app.module.ts
+++ Workshop-router/ng-workshop-componenet/src/app/app.module.ts
@@ -3,28 +3,30 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 import { ThemeListComponent } from './theme-list/theme-list.component';
-import { AsideComponent } from './aside/aside.component';
 import { ThemeListItemComponent } from './theme-list-item/theme-list-item.component';
 import { storageServiceProvider } from './storage.service';
 import { ThemeService } from './theme.service';
 import { UserService } from './user.service';
 import { PostService } from './post.service';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     ThemeListComponent,
-    AsideComponent,
-    ThemeListItemComponent
+    ThemeListItemComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [
     storageServiceProvider,
@@ -32,6 +34,6 @@ import { PostService } from './post.service';
     UserService,
     PostService
   ],
-  bootstrap: [AppComponent,HeaderComponent,FooterComponent]
+  bootstrap: [AppComponent, HeaderComponent, FooterComponent]
 })
 export class AppModule { }
